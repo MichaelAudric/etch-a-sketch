72a38e5e3dbdd6d76b7721c8e1cfa35517785727
Michael <michaelaudric29@gmail.com>
2023-07-16 13:51:55 +0700

    changing container display to flex

--- jsFile.js
+++ jsFile.js
@@ -4,6 +4,7 @@ for (var i = 0; i < 16; i++)
 {
     const container = document.createElement("div");
     container.classList.add("container");
+    container.style.cssText = "display:flex;";
     for (var j = 0; j < 16; j++)
     {
 
