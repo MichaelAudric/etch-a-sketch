2a90791e6bc3cabe77991029fd6f9c18324e9a50
Michael <michaelaudric29@gmail.com>
2023-07-16 15:21:48 +0700

    removing margin top and bottom of container

--- jsFile.js
+++ jsFile.js
@@ -4,7 +4,7 @@ for (var i = 0; i < 16; i++)
 {
     const container = document.createElement("div");
     container.classList.add("container");
-    container.style.cssText = "display:flex; margin : 20px auto; height: 100px;";
+    container.style.cssText = "display:flex; margin : auto; height: 100px;";
     for (var j = 0; j < 16; j++)
     {
         const div = document.createElement("div");
