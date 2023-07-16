6ff71734986421594513555a7ee2808fdac0df14
Michael <michaelaudric29@gmail.com>
2023-07-16 15:16:08 +0700

    assigning 20px auto to container margin

--- jsFile.js
+++ jsFile.js
@@ -4,11 +4,11 @@ for (var i = 0; i < 16; i++)
 {
     const container = document.createElement("div");
     container.classList.add("container");
-    container.style.cssText = "display:flex;";
+    container.style.cssText = "display:flex; margin : 20px auto;";
     for (var j = 0; j < 16; j++)
     {
         const div = document.createElement("div");
-        div.style.cssText = "border : 1px black solid; background color: grey;";
+        div.style.cssText = "border : 1px black solid; background color: red;";
         container.appendChild(div);
     }
     body.appendChild(container);
