610067c2bcddb46f78e34a3b549ed2d628e7df83
Michael <michaelaudric29@gmail.com>
2023-07-16 13:49:59 +0700

    created a div container with class container

--- jsFile.js
+++ jsFile.js
@@ -2,6 +2,8 @@ const body = document.querySelector("body");
 
 for (var i = 0; i < 16; i++)
 {
+    const container = document.createElement("div");
+    container.classList.add("container");
     for (var j = 0; j < 16; j++)
     {
 
