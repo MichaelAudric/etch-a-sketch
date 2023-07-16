6c36b86e1e016aa8b6606a9597232c19f48fb0b2
Michael <michaelaudric29@gmail.com>
2023-07-17 00:08:08 +0700

    removed margin auto from container style

--- jsFile.js
+++ jsFile.js
@@ -11,7 +11,7 @@ for (var i = 0; i < 16; i++)
 {
     const container = document.createElement("div");
     container.classList.add("container");
-    container.style.cssText = "display:flex; margin : auto; height: 50px; width: 800px";
+    container.style.cssText = "display:flex; margin : auto; height: 50px;";
     for (var j = 0; j < 16; j++)
     {
         const div = document.createElement("div");
