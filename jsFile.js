8d79f956a66916f773654ae5a7466ae500f9f3d5
Michael <michaelaudric29@gmail.com>
2023-07-17 00:22:51 +0700

    changed container of containers width to 800 and changed container height to 800/n pixel

--- jsFile.js
+++ jsFile.js
@@ -1,20 +1,20 @@
 const body = document.querySelector("body");
 
-const n = prompt ("input how many grid do you want in a row:");
+const n = prompt ("input how many grid do you want in a row (input below 100 for best performance):");
 
 function getRandomColor(){
     return "#" + (Math.floor(Math.random()*16777215).toString(16));
 }
 
 const container_of_containers = document.createElement("div");
-container_of_containers.style.cssText = "border : 1px black solid; width : 960px; margin: auto;";
+container_of_containers.style.cssText = "border : 1px black solid; width : 800px; margin: auto;";
 
 for (var i = 0; i < n; i++)
 {
     const container = document.createElement("div");
     container.classList.add("container");
     container.style.cssText = "display:flex; margin : auto;";
-    container.style.height = (960/n).toString()+"px";
+    container.style.height = (800/n).toString()+"px";
     for (var j = 0; j < n; j++)
     {
         const div = document.createElement("div");
